cdee38869a2ddc2bbb6cad17910c9b2e2af452c3
NagashLL <konrad.lipiec@student.nhlstenden.com>
2023-04-19 13:12:56 +0700

    adding the calendar module

--- parse-to-ics.py
+++ parse-to-ics.py
@@ -1,1 +1,11 @@
-print ('Hello World!')
+
+
+# imports
+from icalendar import Calendar, Event, vCalAddress, vText
+from datetime import datetime
+from pathlib import Path
+import os
+import pytz
+ 
+# init the calendar
+cal = Calendar()
